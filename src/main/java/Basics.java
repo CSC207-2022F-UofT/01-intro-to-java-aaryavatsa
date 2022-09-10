12063a0fa31e8a2107e388826b6f29cd5a774220
aaryavatsa <aaryavatsa@gmail.com>
2022-09-10 16:22:04 -0400

    Try 1

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -44,6 +44,7 @@ public class Basics {
          *                Hello World!
          */
 
+        System.out.println("Hello World!");
 
 
         /* 4. In Python, we could declare variables using a simple assignment
@@ -62,6 +63,8 @@ public class Basics {
          * TODO (Task 2): Create a variable named my_variable and assign it the
          *                value 100.
          */
+
+        int my_variable = 100;
 
 
 
@@ -100,6 +103,9 @@ public class Basics {
          * Current count: 0
          */
 
+        for (int i = 10; i >= 0; i--) {
+            System.out.println("Current count: " + i);
+        }
 
     }
 
@@ -144,6 +150,12 @@ public class Basics {
 
         // Fill in the rest of the body here
 
+        String[] parts = to_split.split(" ");
+
+        for (int i = 0; i < 7; i++) {
+            ret.append(parts[i].charAt(0));
+        }
+
         return ret.toString();
     }
 
@@ -171,6 +183,9 @@ public class Basics {
          *                (e.g. arr[i] gives you the item at index i).
          */
 
+        for (int i = 1; i < arr.length; i = i + 2) {
+            current_sum = current_sum + arr[i];
+        }
         return current_sum;
     }
 
